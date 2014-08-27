61f6fe87d1e314a107ac3d700235d6fe4a3b437a
oredi <darren_seet@oredi.com>
2014-08-28 01:18:58 +0900

    Adding stylus

--- Multivision/server.js
+++ Multivision/server.js
@@ -1,14 +1,30 @@
 /**
  * Created by darren on 8/28/14.
  */
-var express = require('express');
+var express = require('express')
+    , stylus = require('stylus')
+    , logger = require('morgan')
+    , bodyParser = require('body-parser');
 
 var env = process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+
+
 
 var app = express();
 
 app.set('views', __dirname + '/server/views');
 app.set('view engine', 'jade');
+app.use(logger('dev'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(stylus.middleware(
+    {
+        src: __dirname + '/public',
+        compile: function (str, path) {
+            return stylus(str).set('filename', path);
+        }
+    }
+));
+app.use(express.static(__dirname + '/public'));
 
 app.get('*', function (req, res) {
     res.render('index');
