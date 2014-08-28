811e7922783e41b66d2566bcccd73a00a18e3b1e
oredi <nakagin@803>
2014-08-28 11:56:51 +0900

    Preparing for Login

--- Multivision/server/config/mongoose.js
+++ Multivision/server/config/mongoose.js
@@ -1,9 +1,48 @@
 var mongoose = require('mongoose');
+
+
 module.exports = function(config) {
     mongoose.connect(config.db);
     var db = mongoose.connection;
     db.on('error', console.error.bind(console, 'connection error....'));
     db.once('open', function () {
         console.log('multivision db opened');
+    });
+
+    var userSchema = mongoose.Schema({
+        local: {
+            firstName: String
+            , lastName: String
+            , userName: String
+        }
+    });
+
+    var User = mongoose.model('User', userSchema);
+    User.find({}, function (err, collection) {
+        if(collection.length === 0) {
+            User.create({
+                local:{
+                    firstName: 'Darren'
+                    , lastName: 'Seet'
+                    , userName: 'darren'
+                }
+            });
+
+            User.create({
+                local:{
+                    firstName: 'John'
+                    , lastName: 'Papa'
+                    , userName: 'john'
+                }
+            });
+
+            User.create({
+                local:{
+                    firstName: 'Dan'
+                    , lastName: 'Wahlin'
+                    , userName: 'dan'
+                }
+            });
+        }
     });
 };
