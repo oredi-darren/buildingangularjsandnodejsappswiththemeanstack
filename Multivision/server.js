1c33d710e43567c22b23f1271a7f49ef586faaa3
oredi <nakagin@803>
2014-08-28 11:13:30 +0900

    Adding a login display

--- Multivision/server.js
+++ Multivision/server.js
@@ -37,8 +37,8 @@ var messageSchema = mongoose.Schema({message: String});
 var Message = mongoose.model('Messages', messageSchema);
 
 app.use(express.static(__dirname + '/public'));
-app.get('/partials/:partialPath', function (req, res) {
-    res.render('partials/' + req.params.partialPath);
+app.get('/partials/*', function (req, res) {
+    res.render('../../public/app/' + req.params[0]);
 });
 app.get('*', function (req, res) {
     res.render('index');
