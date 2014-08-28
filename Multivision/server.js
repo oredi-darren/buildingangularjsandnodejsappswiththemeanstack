46d0d469467e1943d9d19df8b1ae33672e1c27e2
oredi <nakagin@803>
2014-08-28 13:43:05 +0900

    Creating the Server side Login code

--- Multivision/server.js
+++ Multivision/server.js
@@ -1,13 +1,46 @@
 /**
  * Created by darren on 8/28/14.
  */
-var express = require('express');
+var express = require('express')
+    , mongoose = require('mongoose')
+    , passport = require('passport')
+    , LocalStrategy = require('passport-local').Strategy;
 
 var env = process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 var app = express();
 var config = require('./server/config/config')[env];
 require('./server/config/express')(app, config);
 require('./server/config/mongoose')(config);
+
+var User = mongoose.model('User');
+passport.use(new LocalStrategy(
+    function (username, password, done) {
+       User.findOne({local: { username: username }}, function (err, user) {
+           if(user) {
+               return done(null, user);
+           }
+
+           return done(null, false);
+       });
+    }
+));
+
+passport.serializeUser(function (user, done) {
+    if(user) {
+        done(null, user._id);
+    }
+});
+
+passport.deserializeUser(function (id, done) {
+    User.findOne({_id: id}, function (err, user) {
+        if(user) {
+            return done(null, user);
+        }
+
+        return done(null, false);
+    });
+});
+
 require('./server/config/routes')(app);
 
 app.listen(config.port);
--- Multivision/server/config/express.js
+++ Multivision/server/config/express.js
@@ -1,13 +1,21 @@
 var express = require('express')
     , stylus = require('stylus')
     , logger = require('morgan')
-    , bodyParser = require('body-parser');
+    , bodyParser = require('body-parser')
+    , cookieParser = require('cookie-parser')
+    , session = require('express-session')
+    , passport = require('passport');
+
 
 module.exports = function(app, config) {
     app.set('views', config.rootPath + '/server/views');
     app.set('view engine', 'jade');
     app.use(logger('dev'));
+    app.use(cookieParser());
     app.use(bodyParser.urlencoded({ extended: false }));
+    app.use(session({ secret: 'multi vision unicorns' }));
+    app.use(passport.initialize());
+    app.use(passport.session());
     app.use(stylus.middleware(
         {
             src: config.rootPath + '/public',
