9586afa95a6aedbf41f608cd99c083f918b7b073
oredi <darren_seet@oredi.com>
2014-08-29 03:32:45 +0900

    Protecting Admin Pages on the Client

--- Multivision/public/app/app.js
+++ Multivision/public/app/app.js
@@ -3,10 +3,32 @@
  */
 angular.module('app', ['ngResource', 'ngRoute']);
 angular.module('app').config(function ($routeProvider, $locationProvider) {
+    var routeRoleChecks = {
+        admin: {
+            auth: function (mvAuth) {
+                return mvAuth.authorizedCurrentUserForRoute('admin');
+            }
+        }
+    };
+
     $locationProvider.html5Mode(true);
     $routeProvider
         .when('/', {
             templateUrl: '/partials/main/main'
             , controller: 'mvMainCtrl'
+        })
+        .when('/admin/users', {
+            templateUrl: '/partials/admin/user-list'
+            , controller: 'mvUserListCtrl'
+            , resolve: routeRoleChecks.admin
         });
+});
+
+angular.module('app').run(function ($rootScope, $location) {
+    $rootScope.$on('$routeChangeError', function (evt, current, previous, rejection) {
+        console.log(rejection);
+        if(rejection === 'not authorized') {
+            $location.path('/');
+        }
+    });
 });
