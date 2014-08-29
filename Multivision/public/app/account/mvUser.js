a14e2927f4cc0e69844a720f488ff6abebbd3519
oredi <nakagin@803>
2014-08-29 15:34:36 +0900

    Course List - Creating the Client-side code

--- Multivision/public/app/account/mvUser.js
+++ Multivision/public/app/account/mvUser.js
@@ -4,7 +4,7 @@ angular.module('app').factory('mvUser', function ($resource) {
     });
     UserResource.prototype.isAdmin = function () {
         return this.roles && this.roles.indexOf('admin') > -1;
-    }
+    };
 
     return UserResource;
 });
--- Multivision/public/app/app.js
+++ Multivision/public/app/app.js
@@ -35,6 +35,10 @@ angular.module('app').config(function ($routeProvider, $locationProvider) {
             templateUrl: '/partials/account/profile'
             , controller: 'mvProfileCtrl'
             , resolve: routeRoleChecks.user
+        })
+        .when('/courses', {
+            templateUrl: '/partials/courses/course-list'
+            , controller: 'mvCourseListCtrl'
         });
 });
 
